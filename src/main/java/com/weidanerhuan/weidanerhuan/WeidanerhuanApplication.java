2c4c57c7697de178bbbfab68766bd4603c4e7592
陈文威 <chenwenwei@gc.com.cn>
2018-10-28 11:02:01 +0800

    Initial commit

---
+++ src/main/java/com/weidanerhuan/weidanerhuan/WeidanerhuanApplication.java
@@ -0,0 +1,12 @@
+package com.weidanerhuan.weidanerhuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeidanerhuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeidanerhuanApplication.class, args);
+    }
+}
---
+++ src/test/java/com/weidanerhuan/weidanerhuan/WeidanerhuanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.weidanerhuan.weidanerhuan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeidanerhuanApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
